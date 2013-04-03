2ef46bd70b72817a2728474212c53fff3201586a
Ondrej Sika <sika.ondrej@gmail.com>
2013-04-03 21:38:58 +0200

    add participant admin table

--- apps/bmxacademy/admin.py
+++ apps/bmxacademy/admin.py
@@ -6,8 +6,12 @@ from image_cropping import ImageCroppingMixin
 class NewAdmin(ImageCroppingMixin, admin.ModelAdmin):
     pass
 
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "birth_number", "address", "phone",
+        "email", "tshirt_size", "cap_size", "camp_variant", "transfer")
+
 admin.site.register(New, NewAdmin)
 admin.site.register(Video)
-admin.site.register(Participant)
+admin.site.register(Participant, ParticipantAdmin)
 admin.site.register(Message)
 admin.site.register(Email)
