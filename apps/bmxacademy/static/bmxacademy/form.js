107c69efb2372e0818686e27604abb4873fb4b1c
Ondrej Sika <sika.ondrej@gmail.com>
2013-04-18 13:05:52 +0200

    fix form.js ad ie rewrite

--- apps/bmxacademy/static/bmxacademy/form.js
+++ apps/bmxacademy/static/bmxacademy/form.js
@@ -26,3 +26,61 @@ function init_form(form_url, formid, submitid, domid){
         });
     });
 }
+
+function init_form2(form_url){
+    $(document).ready(function(){
+        // $("input").keypress(function(e){
+        //     console.log(e);
+        //     if (e.which == 13) {
+        //         form_request();
+        //         return false;
+        //     }
+        // });
+
+        done = function (data) {
+            if(data.indexOf("redirect:") !== -1) document.location.href = data.split(":")[1];
+            else {
+                $("#reg_div").html(data);
+                $("#reg_sub").click(form_request); 
+            }
+        }
+
+        function form_request() {
+            django_remote_ajax.post(form_url, done, $("#reg_form").serialize());
+        }
+
+        django_remote_ajax.get(form_url, function (data) {
+            $("#reg_div").html(data);
+            $("#reg_sub").click(form_request);
+        });
+    });
+}
+
+function init_form3(form_url){
+    $(document).ready(function(){
+        // $("input").keypress(function(e){
+        //     console.log(e);
+        //     if (e.which == 13) {
+        //         form_request();
+        //         return false;
+        //     }
+        // });
+
+        done3 = function (data) {
+            if(data.indexOf("redirect:") !== -1) document.location.href = data.split(":")[1];
+            else {
+                $("#con_div").html(data);
+                $("#con_sub").click(form_request3); 
+            }
+        }
+
+        function form_request3() {
+            django_remote_ajax.post(form_url, done3, $("#con_form").serialize());
+        }
+
+        django_remote_ajax.get(form_url, function (data) {
+            $("#con_div").html(data);
+            $("#con_sub").click(form_request3);
+        });
+    });
+}
